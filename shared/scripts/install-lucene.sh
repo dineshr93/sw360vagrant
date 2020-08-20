f35484acb50386a80028ee21fce7cb56c93e2d84
dineshr93 <dineshr93@gmail.com>
2020-08-20 22:39:39 +0530

    Update install-lucene.sh

--- shared/scripts/install-lucene.sh
+++ shared/scripts/install-lucene.sh
@@ -21,7 +21,7 @@ echo "-[shell provisioning] start installing couchdb-lucene ..."
 /bin/tar xvzf /requiredswforsw360/packages/couchdb-lucene.tar.gz -C /tmp
 cp --remove-destination /requiredswforsw360/packages/couchdb-lucene.ini /tmp/couchdb-lucene-1.0.2/src/main/resources/
 pushd /tmp/couchdb-lucene-1.0.2
-patch -p1 </requiredswforsw360/couchdb-lucene.patch
+patch -p1 </requiredswforsw360/packages/couchdb-lucene.patch
 mvn clean install war:war
 popd
 cp --remove-destination /tmp/couchdb-lucene-1.0.2/target/couchdb-lucene-*.war /opt/liferay-ce-portal-7.2.1-ga2/tomcat-9.0.17/webapps/couchdb-lucene.war
