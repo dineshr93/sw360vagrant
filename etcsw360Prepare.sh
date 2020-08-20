89f39fdce41286cbafe71846a8f6d6c130fabcdc
dineshr93 <dineshr93@gmail.com>
2020-08-21 02:17:52 +0530

    Update etcsw360Prepare.sh

--- etcsw360Prepare.sh
+++ etcsw360Prepare.sh
@@ -4,6 +4,7 @@ cd /etc/sw360
 wget 'https://github.com/dineshr93/sw360/raw/master/build-configuration/resources/couchdb.properties'
 wget 'https://github.com/dineshr93/sw360/raw/master/build-configuration/resources/search.properties'
 wget 'https://github.com/dineshr93/sw360/raw/master/build-configuration/resources/sw360.properties'
+wget 'https://github.com/dineshr93/sw360/raw/10.0.0-M1/build-configuration/resources/log4j.properties'
 echo "Created etc/sw360 folder & populated!"
 sudo mv /etc/couchdb/local.ini /etc/couchdb/.localini
 
